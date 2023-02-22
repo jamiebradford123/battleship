4caf84ae756f470bdb5fa62c586a3a028b71cf5d
jamiebradford123 <jamiebradford123@hotmail.com>
2023-02-22 00:22:16 +0000

    add intro and rules to game

--- run.py
+++ run.py
@@ -1,3 +1,30 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+rules = """
+- The aim of the game is to find all of your opponents ships before they find
+yours.
+- This game is YOU vs COMPUTER
+- Both players have 4 ships to find on a 5x5 grid
+- Enter a row number between 0-4 , with 0 being the far left, and right being 4
+- Enter a column number between 0-4 , with 0 being the far left, and right
+being 4
+- If a ship is hit, an '*' will appear on the board
+- If a guess is missed, an 'X' will appear on the board
+- Each player takes it in turns
+
+Best of Luck!
+
+"""
+
+
+def new_game():
+    """
+    Starts a new game. Sets the board sixe and number of ships. Resets the
+    scores and randomizes the board
+    """
+    print("Welcome to Battleships!\n")
+    print("Here are the rules:")
+    print(rules)
+
+
+new_game()
