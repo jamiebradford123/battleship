438e8f039eed768c169d1f92c75d3a3e83bfc8be
jamiebradford123 <jamiebradford123@hotmail.com>
2023-03-24 15:28:00 +0000

    fix problems in problems tab

--- run.py
+++ run.py
@@ -6,11 +6,11 @@ rules = """
 run out of missiles
 - You have 4 ships to find with 12 missiles on a 5x5 grid
 - Enter a row number between 1-5 , with 1 being the top and 5 being the bottom
-- Enter a column between A-E , with A being the far left and E being the far right
+- Enter a column between A-E , A being far left and E being the far right
 being 4
 - If a ship is hit, an 'X' will appear on the board
 - If a guess is missed, an '-' will appear on the board
-- If you run out of missiles before all the ships are destroyed, you loose
+- If you run out of missiles before all the ships are destroyed, you lose
 - If you destroy all the ships, you win!
 
 Best of Luck!
@@ -18,6 +18,10 @@ Best of Luck!
 
 
 def intro():
+    """ 
+    Introduction to the game, asks players if they need to see the rules.
+    Also contains an input to start the game
+    """
     print("Welcome to Battleships!\n")
     show_rules = input("\nWould you like to see the rules? Y/N \n").upper()
     if show_rules == "N":
@@ -89,10 +93,10 @@ class Battleship:
                 if int(x_row) in [1, 2, 3, 4, 5]:
                     break
                 else:
-                    print("Enter a number 1-5")
+                    print("Invalid! You must enter a number 1-5")
                     continue
             except:
-                print("Enter a number 1-5")
+                print("Invalid! You must enter a number 1-5")
                 continue
 
         while True:
@@ -101,20 +105,19 @@ class Battleship:
                 if y_column in ['A', 'B', 'C', 'D', 'E']:
                     break
                 else:
-                    print("Enter a letter A-E")
+                    print("Invalid! You must enter a letter A-E")
                     continue
             except:
-                print("Enter a letter A-E")
+                print("Invalid! You must enter a letter A-E")
                 continue
         time.sleep(0.5)
         print("Firing now...\n")
         time.sleep(1)
         return int(x_row) - 1, Board.get_letters_to_numbers()[y_column]
 
-
     def count_hit_ships(self):
         """
-        Counts the number of hit ships 
+        Counts the number of hit ships
         """
         hit_ships = 0
         for row in self.board:
@@ -122,8 +125,9 @@ class Battleship:
                 if column == "X":
                     hit_ships += 1
         return hit_ships
-        
-def RunGame():
+
+
+def run_game():
     """
     Runs the game
     """
@@ -136,7 +140,7 @@ def RunGame():
         print("Fire away!\n")
     else:
         print("Invalid input")
-        return RunGame()
+        return run_game()
     while turns > 0:
         Board.print_board(user_guess_board)
         user_x_row, user_y_column = Battleship.get_user_input(object)
@@ -160,6 +164,7 @@ def RunGame():
                 Board.print_board(user_guess_board)
                 break
 
+
 if __name__ == '__main__':
     intro()
-    RunGame()
+    run_game()
